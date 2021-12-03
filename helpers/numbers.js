8c047cef3c9167bdcf0f745d48e52f95bf005fe9
blugee <blugee>
2021-12-03 20:20:19 +0530

    Changes for env

--- helpers/numbers.js
+++
@@ -1,9 +0,0 @@
-exports.getPrice = function(num){
-  var price = (num/100).toFixed(2);
-  return isNaN(price) ? 0 : price;
-}
-
-exports.setPrice = function(num){
-  var price = num*100;
-  return isNaN(price) ? 0 : price;
-}
